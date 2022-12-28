a33158d53cfc1e048f12c16e295f7f09690643b7
Evros Nireas <ronnietheengineer@gmail.com>
2022-12-28 18:52:45 +0000

    newline

--- postfixinstall.sh
+++ postfixinstall.sh
@@ -7,8 +7,8 @@ sudo hostnamectl set-hostname mail.letsdoiton.cloud
 # Install Postfix
 sudo apt-get update
 
-echo "postfix postfix/mailname string letsdoiton.cloud" | debconf-set-selections
-echo "postfix postfix/main_mailer_type string 'Internet Site'" | debconf-set-selections
+sudo echo "postfix postfix/mailname string letsdoiton.cloud" | debconf-set-selections
+sudo echo "postfix postfix/main_mailer_type string 'Internet Site'" | debconf-set-selections
 
 sudo apt-get install postfix -y
 
