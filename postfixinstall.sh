ded9a04cded0a8841d1ee0eead19dc90d5c0fc22
Evros Nireas <ronnietheengineer@gmail.com>
2022-12-30 22:45:22 +0000

    new line added

--- postfixinstall.sh
+++ postfixinstall.sh
@@ -2,7 +2,7 @@
 
 # Change the hostname
 
-sudo hostnamectl set-hostname mail.letsdoiton.cloud
+#sudo hostnamectl set-hostname mail.letsdoiton.cloud
 
 # Install Postfix
 sudo apt-get update
@@ -11,6 +11,14 @@ sudo echo "postfix postfix/mailname string letsdoiton.cloud" | debconf-set-selec
 sudo echo "postfix postfix/main_mailer_type string 'Internet Site'" | debconf-set-selections
 
 sudo apt-get install postfix -y
+
+#Backup the existing conf file
+
+sudo cp /etc/opendkim.conf /etc/opendkim.conf.bak
+
+#Add new line after Syslog
+
+sed -i '/Syslog/a Logwhy yes' /etc/opendkim.conf 
 
 # Install Telnet Client
 
