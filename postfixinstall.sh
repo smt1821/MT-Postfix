6c032dc25b0ce8285d05083eb79924820ed8288b
MT <merictabakoglu@gmail.com>
2023-02-22 10:51:13 +0000

    line changed

--- postfixinstall.sh
+++ postfixinstall.sh
@@ -4,32 +4,30 @@
 
 #sudo hostnamectl set-hostname mail.letsdoiton.cloud
 
+
 # Install Postfix
 sudo apt-get update
-
-sudo echo <<< "postfix postfix/mailname string smtptest | debconf-set-selections"
-sudo echo <<< "postfix postfix/main_mailer_type string 'Internet Site'" | debconf-set-selections
-
-sudo apt-get install postfix -y
-
-#Backup the existing conf file
-
-sudo cp /etc/opendkim.conf /etc/opendkim.conf.bak
-
-#Add new line after Syslog
-
-#sed -i '/Syslog/a Logwhy yes' /etc/opendkim.conf 
 
 # Install Telnet Client
 
 sudo apt install telnet -y
+
+# Install postfix
+
+sudo echo <<< "postfix postfix/mailname string intelex.dev | debconf-set-selections"
+sudo echo <<< "postfix postfix/main_mailer_type string 'Internet Site'" | debconf-set-selections
+
+sudo apt-get install postfix -y
+
 
 # Setting up DKIM
 
 sudo apt install opendkim opendkim-tools -y
 sudo gpasswd -a postfix opendkim
 
+#Backup the existing conf file
 
+sudo cp /etc/opendkim.conf /etc/opendkim.conf.bak
 ################################################################
 #### Edit /etc/opendkim.conf add "Logwhy yes" after "Syslog yes" line
 #### Uncomment the following "Canonicalization simple" line change simple with "relaxed/simple"
