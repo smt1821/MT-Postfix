e0a7de43324c22664678e8c2fabfea6df27140aa
MT <ronnietheengineer@gmail.com>
2025-02-22 23:56:13 +0000

    name removed

--- mailflush.sh
+++ mailflush.sh
@@ -1,3 +1,3 @@
-for id in $(postqueue -p | grep -B1 "KRIS.GILES@sundyne.com" | grep -Eo '^[0-9A-F]{10,}'); do
+for id in $(postqueue -p | grep -B1 "noname@noname.com" | grep -Eo '^[0-9A-F]{10,}'); do
     postsuper -r $id
 done
